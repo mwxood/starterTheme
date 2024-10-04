ed3108b8f94911716d4502f2196eaf1acf0f5021
Mihail <mwxood@users.noreply.github.com>
2024-10-04 12:12:46 +0300

    init

--- index.php
+++ index.php
@@ -2,29 +2,14 @@
 
 defined('ABSPATH') || exit;
 
-?>
+get_header(); ?>
 
-<?php get_header(); ?>
+<?php if(have_posts ()):  the_post (); ?>
 
+    <?php the_title('<h2>', '</h2>'); ?>
 
-    <div class="container container--narrow page-section">
-      <?php
-      while(have_posts()) {
-          the_post(); ?>
-        <div class="post-item">
-          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-          <div class="metabox">
-            <p>Posted by: <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-          </div>
+    <?php the_content(); ?>
 
-          <div class="generic-content">
-            <?php the_excerpt(); ?>
-            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
-          </div>
-        </div> 
-      <?php }?>
-        
-      <?php echo paginate_links(); ?> 
-    </div>
+<?php endif; ?>
+
 <?php get_footer(); ?>
-
