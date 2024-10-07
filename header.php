c2890e48e859e668fb74da28727094c403e1290d
Mihail <mwxood@users.noreply.github.com>
2024-10-07 12:26:52 +0300

    init

--- header.php
+++ header.php
@@ -12,3 +12,15 @@ defined('ABSPATH') || exit;
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        logo
+      </a>
+
+      <?php get_template_part('template-parts/header/header-menu');  ?>
+    
+    </header>
+
+</div><!-- end container -->
